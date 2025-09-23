ccb6602d27b16e24e7f4747530127170cdc67205
pratik pithiya <pithiyapratik13@gmail.com>
2025-09-23 18:03:00 +0530

    resolve issue

--- Frontend/src/Store/store.js
+++ Frontend/src/Store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from './Slices/userSlice.js'
-import postSlice from './Slices/postSlice'
+import postSlice from './Slices/postSlice.js'
 //import umSlice from './umSlice.js'
 import storyReducer from "./Slices/storySlice.js"
-import followReducer from './Slices/followSlice';
+import followReducer from './Slices/followSlice.js';
 
 
 const Store = configureStore({
